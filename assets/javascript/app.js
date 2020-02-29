03799c2a924fdc2045cacfb9f54b843d29f9c775
Clayton Hill <clayman0123@yahoo.com>
2020-02-28 20:28:40 -0700

    done button and timeout working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,28 @@ var timer = 10;
 
 $(document).ready(function() {
     $("#game").hide();
+    $("#stats").hide();
+});
+
+$("#done").click(function() {
+    $("#game").hide();
+    $("#stats").show();
 });
 
 $("#start").click(function() {
     $("#start").hide();
     $("#rules").hide();
+    $("#stats").hide();
     $("#game").show();
     setInterval(function() {
         timer--;
-        if(timer >= 0) {
+        if(timer > 0) {
             $("#timer").text("Time Remaining: " + timer + " Seconds");
         }
-    
-        if(timer == 0) {
         
-        }
-    }, 1000);
-    
+        else {
+            $("#game").hide();
+            $("#stats").show();
+            }       
+    }, 1000);    
 });
