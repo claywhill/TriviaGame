63cbd01cb7c49f76e8b6a450c1fb845ab67139a6
Clayton Hill <clayman0123@yahoo.com>
2020-02-28 19:10:31 -0700

    added .hide/.show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,23 @@
 var timer = 10;
 
-setInterval(function() {
-    timer--;
-    if(timer >= 0) {
-        $("#timer").text("Time Remaining: " + timer + " Seconds");
-    }
 
-    if(timer == 0) {
+$(document).ready(function() {
+    $("#game").hide();
+});
+
+$("#start").click(function() {
+    $("#start").hide();
+    $("#rules").hide();
+    $("#game").show();
+    setInterval(function() {
+        timer--;
+        if(timer >= 0) {
+            $("#timer").text("Time Remaining: " + timer + " Seconds");
+        }
     
-    }
-}, 1000);
+        if(timer == 0) {
+        
+        }
+    }, 1000);
+    
+});
